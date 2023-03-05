93c10e7e6c82fa13978c9e61d2713b22e36deb47
Yuri <yuriyra@gmail.com>
2023-03-05 17:48:55 -0500

    hello. This is my first code

--- TesteBahYurixCode/TesteBahYurixCode/main.swift
+++ TesteBahYurixCode/TesteBahYurixCode/main.swift
@@ -5,7 +5,14 @@
 //  Created by Barbara Silva on 2023-03-05.
 //
 
-import Foundation
+import Cocoa
 
-print("Hello, World!")
 
+func showWelcome() {
+    print("Welcome to my app!")
+    print("By default This prints out a conversion")
+    print("chart from centimeters to inches, but you")
+    print("can also set a custom range if you want.")
+}
+
+showWelcome()
